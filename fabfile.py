fba3fe54c13d8f50a3592c0b37d33f8451fe9955
Andres Vargas <andres@python3.ninja>
2021-04-05 23:50:03 -0500

    easy deploy

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,9 @@
 from invoke import run as local
 from fabric import task
+from patchwork.transfers import rsync
+
+exclude_dirs = [".git", "node_modules", ".cache", ".github", "db.sqlite3",
+                ".env"]
 
 
 @task
@@ -17,5 +21,16 @@ def test(c):
 
 
 @task
-def deploy(c):
-    c.run("id")
+def deploy(ctx):
+    local("yarn install", echo=True)
+    local("yarn run build", echo=True)
+    local("python manage.py collectstatic --noinput", echo=True)
+    local("find . -name '__pycache__' |xargs rm -rf ", echo=True)
+    rsync(ctx, ".", "apps/spy_agency", exclude=exclude_dirs)
+    with ctx.cd("apps/spy_agency"):
+        with ctx.prefix("source ~/apps/spy_agency/.env/bin/activate"):
+            ctx.run("pip install -r requirements.in")
+            ctx.run("python manage.py migrate")
+    ctx.run("sudo supervisorctl restart spy-agency")
+
+
